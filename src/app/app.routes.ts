4d93a5baa6394c288edab21e30a5a6d7ebb650a0
Carlos Adriano <c.adriano.silva@hotmail.com>
2025-08-25 20:32:00 -0300

    wip: add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Home } from './pages/home/home';
 import { ProductDetail } from './pages/product-detail/product-detail';
 import { Login } from './pages/login/login';
 import { Cart } from './pages/cart/cart';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'product/:id', component: ProductDetail },
   { path: 'login', component: Login },
-  { path: 'cart', component: Cart },
+  { path: 'cart', component: Cart, canActivate: [authGuard] },
 ];
